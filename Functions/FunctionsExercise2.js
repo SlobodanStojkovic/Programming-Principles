8f95e5c86aed1d5c83d4a0a6393425b757131356
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-04-05 19:01:44 +0200

    Updated FunctionsExercise2

--- Functions/FunctionsExercise2.js
+++ Functions/FunctionsExercise2.js
@@ -7,11 +7,17 @@
 ======================================================================================
 */
 
+function isString(value) {
+    var result = "";
+    if (typeof (value) === "string") {
+        result = "true";
+    } else {
+        result = "false";
+    } return result;
+}
 
-
-
-
-
+console.log(isString("My random string"));
+console.log(isString(12));
 
 
 
@@ -27,9 +33,19 @@ false -> false
 */
 
 
+function isStringBlank(value) {
+    var result = ""
+    if (typeof (value) === "string" && value === " ") {
+        result = "true";
+    } else {
+        result = "false";
+    } return result;
+}
 
-
-
+console.log(isStringBlank(" "));
+console.log(isStringBlank(12));
+console.log(isStringBlank(false));
+console.log(isStringBlank("Is this a blank string?"));
 
 
 
@@ -37,16 +53,21 @@ false -> false
 /*
 ======================================================================================
 3. Write a function that concatenates a given string n times (default is 1).
-	"Ha" -> "Ha"
+    "Ha" -> "Ha"
 "Ha", 3 -> "HaHaHa"
 ======================================================================================
 */
 
+function concatenateTimes(string, times) {
+    var result = ""; {
+        while (times > 0) {
+            result += string;
+            times--;
+        }
+    } return result;
+}
 
-
-
-
-
+console.log(concatenateTimes("Ha", 3));
 
 
 
@@ -69,8 +90,8 @@ false -> false
 
 /*
 ======================================================================================
-5. Write a function to find the position of the first occurrence of a character in a string. 
-The result should be the position of character. If there are no occurrences of the character, 
+5. Write a function to find the position of the first occurrence of a character in a string.
+The result should be the position of character. If there are no occurrences of the character,
 the function should return -1.
 ======================================================================================
 */
@@ -86,8 +107,8 @@ the function should return -1.
 
 /*
 ======================================================================================
-6. Write a function to find the position of the last occurrence of a character in a string. 
-The result should be in human numeration form. If there are no occurrences of the character, 
+6. Write a function to find the position of the last occurrence of a character in a string.
+The result should be in human numeration form. If there are no occurrences of the character,
 function should return -1.
 ======================================================================================
 */
@@ -103,7 +124,7 @@ function should return -1.
 
 /*
 ======================================================================================
-7. Write a function to convert string into an array. Space in a string should be 
+7. Write a function to convert string into an array. Space in a string should be
 represented as “null” in new array.
 
 "My random string" -> ["M", "y", null, "r", "a"]
@@ -122,8 +143,8 @@ represented as “null” in new array.
 
 /*
 ======================================================================================
-8. Write a function that accepts a number as a parameter and checks if the number is 
-prime or not. Note: A prime number (or a prime) is a natural number greater than 1 
+8. Write a function that accepts a number as a parameter and checks if the number is
+prime or not. Note: A prime number (or a prime) is a natural number greater than 1
 that has no positive divisors other than 1 and itself.
 ======================================================================================
 */
@@ -140,7 +161,7 @@ that has no positive divisors other than 1 and itself.
 
 /*
 ======================================================================================
-9. Write a function that replaces spaces in a string with provided separator. 
+9. Write a function that replaces spaces in a string with provided separator.
 If separator is not provided, use “-” (dash) as the default separator.
 
     "My random string", "_" -> "My_random_string"
@@ -159,8 +180,8 @@ If separator is not provided, use “-” (dash) as the default separator.
 
 
 /*
-====================================================================================== 
-10. Write a function to get the first n characters and add “...” 
+======================================================================================
+10. Write a function to get the first n characters and add “...”
 at the end of newly created string.
 ======================================================================================
 */
@@ -176,7 +197,7 @@ at the end of newly created string.
 
 /*
 ======================================================================================
-11. Write a function that converts an array of strings into an array of numbers. 
+11. Write a function that converts an array of strings into an array of numbers.
 Filter out all non-numeric values.
 ["1", "21", undefined, "42", "1e+3", Infinity] -> [1, 21, 42, 1000]
 ======================================================================================
@@ -193,9 +214,9 @@ Filter out all non-numeric values.
 
 
 /*
-======================================================================================  
-12. Write a function to calculate how many years there are left until retirement based 
-on the year of birth. Retirement for men is at age of 65 and for women at age of 60. 
+======================================================================================
+12. Write a function to calculate how many years there are left until retirement based
+on the year of birth. Retirement for men is at age of 65 and for women at age of 60.
 If someone is already retired, a proper message should be displayed.
 ======================================================================================
 */
@@ -212,7 +233,7 @@ If someone is already retired, a proper message should be displayed.
 
 /*
 ======================================================================================
-13. Write a function to humanize a number (formats a number to a human-readable string) 
+13. Write a function to humanize a number (formats a number to a human-readable string)
 with the correct suffix such as 1st, 2nd, 3rd or 4th.
 1 -> 1st
 11 -> 11th
