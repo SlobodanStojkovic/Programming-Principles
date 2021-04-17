c2baabe99a555658371a6037f9c1baa27ec58804
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-04-17 18:13:09 +0200

    Use of Self Invoked Functions

---
+++ Functions/useOfSelfInvokedFunctions/script1.js
@@ -0,0 +1,13 @@
+
+(
+    function () {
+        console.log("Hello from script 1")
+        var b = "ddddd"
+    }
+)()
+
+
+
+/*
+These functions are used that they upon calling dont overwrite elements of other function
+*/
---
+++ Functions/useOfSelfInvokedFunctions/script2.js
@@ -0,0 +1,12 @@
+
+(
+    function () {
+        console.log("Hello from script 2")
+        var b = "ddddd"
+    }
+)()
+
+
+/*
+These functions are used that they upon calling dont overwrite elements of other function
+*
